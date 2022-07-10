f92f6a0661bc0244e6ea136da399f48da9e2f7d7
Chris Scrivo <chris.scrivo@gmail.com>
2022-07-10 15:42:48 -0400

    Changed CryptoDetails.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,7 +20,6 @@ const CryptoDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
 
-  if (isLoading) return <Loader />;
   
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
