6b38d2813ef3e2986bc204845dc98ca7d13a28f9
Chris Scrivo <chris.scrivo@gmail.com>
2022-07-10 15:22:06 -0400

    Changed loader CryptoDetails.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,10 +20,7 @@ const CryptoDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
 
-  if (isLoading) return 
-  (
-  <Loader />
-  );
+  if (isLoading) return (  <Loader />  );
   
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
