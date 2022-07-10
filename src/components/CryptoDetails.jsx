3dc9693ba2e26fa7a56826f5ac0c1512e394d2cf
Chris Scrivo <chris.scrivo@gmail.com>
2022-07-10 14:03:38 -0400

    Changes to CryptoDetails.jsx.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,7 +6,7 @@ import { Col, Row, Typography, Select } from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi'
-
+import { Loader } from './Loader';
 import LineChart from './LineChart';
 
 const { Title, Text } = Typography
@@ -20,12 +20,9 @@ const CryptoDetails = () => {
 
   const cryptoDetails = data?.data?.coin;
 
-  if(isFetching) return 'Loading...'
+  if (isLoading) return <Loader />;
   
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
-
-  // console.log(timePeriod)
-  // console.log(Object.values(data?.data?.coin)[11]) // 24hVolume
 
   const stats = [
     { title: 'Price to USD', value: `$ ${data?.data?.coin?.price && millify(data?.data?.coin?.price)}`, icon: <DollarCircleOutlined /> },
@@ -103,7 +100,6 @@ const CryptoDetails = () => {
         </Col>
       </Col>
     </Col>
-    
   )
 }
 
